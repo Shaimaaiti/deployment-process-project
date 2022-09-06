970c62ccb594600f68aedef28bee857cce116879
Shaimaa Abdelhamed <shaimaa.abdelhamed.hassan@gmail.com>
2022-09-06 14:39:44 +0200

    deploy both projects

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
 aws s3 cp --recursive --acl public-read ./www s3://udagramfront/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagramfront/5139724085/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagramfront/
